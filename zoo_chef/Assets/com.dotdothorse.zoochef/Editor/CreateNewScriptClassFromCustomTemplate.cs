76c9ee6a235c44370f153237753ce3c106bfdb6c
Ray Feng <ryfprivate@outlook.com>
2021-08-24 17:11:05 +1000

    Added script template creator

---
+++ zoo_chef/Assets/com.dotdothorse.zoochef/Editor/CreateNewScriptClassFromCustomTemplate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class CreateNewScriptClassFromCustomTemplate
+{
+    private const string pathToYourScriptTemplate = "Assets/com.dotdothorse.zoochef/Scripts/Templates/ZoochefTemplate.cs.txt";
+
+    [MenuItem(itemName: "Assets/Create/Create New Script from Custom Template", isValidateFunction: false, priority: 51)]
+    public static void CreateScriptFromTemplate()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathToYourScriptTemplate, "YourDefaultNewScriptName.cs");
+    }
+}
---
+++ zoo_chef/Assets/com.dotdothorse.zoochef/Scripts/BaseClasses/BaseDescriptionSO.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class BaseDescriptionSO : ScriptableObject
+{
+    [TextArea] public string description;
+}
