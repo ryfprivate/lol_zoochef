68af5d4408da44caedac35abf1759ff8940fdbd1
Ray Feng <ryfprivate@outlook.com>
2021-10-01 20:47:57 +1000

    Changed picky alpaca game to have pause functionality

--- zoo_chef/Assets/com.dotdothorse.zoochef/Scripts/Game/Main/PickyAlpacaGame.cs
+++ zoo_chef/Assets/com.dotdothorse.zoochef/Scripts/Game/Main/PickyAlpacaGame.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3e63fb01a5a3944c0707bf9def1eb2a5596ada40a3cc93916ba32734a1830ef
-size 7956
+oid sha256:f972166ff7ef4fac8d8fc638916f80a930fc35e53220438a993507d89c38ecb7
+size 10388
